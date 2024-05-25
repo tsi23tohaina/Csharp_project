1cb6536ab9e12ea4eb6b6a325909c282cbc4cceb
tsi23tohaina <sitrakatsitohainarajoelisolo@gmail.com>
2024-05-25 04:01:05 +0100

    ajout age de tsitohaina

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿class Program{
     public static void Main(string[] args){
         Console.WriteLine("Hello, World Tsitohaina Rapha!");
+        int i=19;
+        Console.WriteLine("Tu as "+i+"ans");
     }
 }
